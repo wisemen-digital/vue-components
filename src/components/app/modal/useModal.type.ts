29fdf4bcfc3a90e6fcc9224e0ef96c2b2afe85e9
Robbe <robbevaes95@gmail.com>
2023-08-16 09:29:17 +0200

    Removed slots in usemodal for now, for discussion

--- src/components/app/modal/useModal.type.ts
+++ src/components/app/modal/useModal.type.ts
@@ -21,12 +21,13 @@ export type RawProps = VNodeProps & {
 
 export interface ModalSlotOptions { component: Raw<Component>; attrs?: Record<string, any> }
 export type ModalSlot = string | Component | ModalSlotOptions
-
+// TODO - Discuss what we do with slots
+export type PropsWithSlots<P> = Omit<P, 'slots'>
 export interface UseModalOptions<P> {
   modalComponent?: Component
   modalTitle?: string
   component?: Constructor<P>
-  attrs?: (RawProps & P) | (object extends P ? null : never)
+  attrs?: (RawProps & PropsWithSlots<P>) | (object extends P ? null : never)
   slots?: {
     [key: string]: ModalSlot
   }
